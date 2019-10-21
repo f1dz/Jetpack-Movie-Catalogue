1b22ea4e56eb41ac02441622b2f94d9ed2a91a51
Khofidin <offiedz@gmail.com>
2019-10-21 13:59:01 +0700

    Add Instrumen Test MovieFragment

---
+++ app/src/debug/java/in/khofid/moviecatalogue/testing/SingleFragmentActivity.kt
@@ -0,0 +1,37 @@
+package `in`.khofid.moviecatalogue.testing
+
+import `in`.khofid.moviecatalogue.R
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class SingleFragmentActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var content: View = FrameLayout(this)
+        val params = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT, Gravity.CENTER
+        )
+        content.layoutParams = params
+        content.id = R.id.container
+
+        setContentView(content)
+    }
+
+    fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, fragment, "TEST")
+            .commit()
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, fragment)
+            .commit()
+    }
+}
